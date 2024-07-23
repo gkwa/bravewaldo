6b1016e80efb5b272d739bba20b007c16a37df82
Taylor Monacelli <taylormonacelli@gmail.com>
2024-07-23 13:03:02 -0700

    panicing

--- core1/main.go
+++ core1/main.go
@@ -3,6 +3,7 @@ package core
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -14,44 +15,44 @@ import (
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer"
 	"github.com/yuin/goldmark/text"
-	"github.com/yuin/goldmark/util"
 )
 
-type URLWrapperRenderer struct{}
+type URLWrapperRenderer struct {
+	renderer.Renderer
+}
 
 func NewURLWrapperRenderer() renderer.Renderer {
-	r := renderer.NewRenderer()
-	r.AddOptions(renderer.WithNodeRenderers(
-		util.Prioritized(URLWrapperNodeRenderer{}, 100),
-	))
-	return r
+	return &URLWrapperRenderer{
+		Renderer: renderer.NewRenderer(),
+	}
 }
 
-type URLWrapperNodeRenderer struct{}
-
-func (r URLWrapperNodeRenderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
-	reg.Register(ast.KindAutoLink, r.renderAutoLink)
-}
-
-func (r URLWrapperNodeRenderer) renderAutoLink(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	if entering {
-		n := node.(*ast.AutoLink)
-		url := n.URL(source)
-		wrappedURL := fmt.Sprintf("|%s|", url)
-		_, err := w.WriteString(wrappedURL)
+func (r *URLWrapperRenderer) Render(w io.Writer, source []byte, n ast.Node) error {
+	return ast.Walk(n, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
+		if entering {
+			if autoLink, ok := n.(*ast.AutoLink); ok {
+				url := autoLink.URL(source)
+				wrappedURL := fmt.Sprintf("|%s|", url)
+				_, err := w.Write([]byte(wrappedURL))
+				if err != nil {
+					return ast.WalkStop, err
+				}
+				return ast.WalkSkipChildren, nil
+			}
+		}
+		err := r.Renderer.Render(w, source, n)
 		if err != nil {
 			return ast.WalkStop, err
 		}
-		return ast.WalkSkipChildren, nil
-	}
-	return ast.WalkContinue, nil
+		return ast.WalkContinue, nil
+	})
 }
 
+func (r *URLWrapperRenderer) AddOptions(...renderer.Option) {}
+
 func Example(logger logr.Logger) {
-	if logger.V(1).Enabled() {
-		logger.V(1).Info("Debug: Entering Example function")
-	}
-	logger.V(1).Info("Processing markdown file")
+	logger.V(1).Info("Debug: Entering Example function")
+	logger.Info("Processing markdown file")
 
 	source, err := os.ReadFile("testdata/input.md")
 	if err != nil {
@@ -68,14 +69,12 @@ func Example(logger logr.Logger) {
 	)
 	doc := md.Parser().Parse(text.NewReader(source))
 
-	if logger.V(1).Enabled() {
-		initialAST, err := dumpAST(doc)
-		if err != nil {
-			logger.Error(err, "Failed to dump initial AST")
-			return
-		}
-		logger.V(1).Info("Initial AST structure", "structure", initialAST)
+	initialAST, err := dumpAST(doc)
+	if err != nil {
+		logger.Error(err, "Failed to dump initial AST")
+		return
 	}
+	logger.V(1).Info("Initial AST structure", "structure", initialAST)
 
 	var urls []string
 	err = ast.Walk(doc, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
@@ -83,17 +82,13 @@ func Example(logger logr.Logger) {
 			return ast.WalkContinue, nil
 		}
 
-		if logger.V(1).Enabled() {
-			logger.V(1).Info("Walking node", "type", fmt.Sprintf("%T", n), "kind", n.Kind())
-		}
+		logger.V(1).Info("Walking node", "type", fmt.Sprintf("%T", n), "kind", n.Kind())
 
 		if autoLink, ok := n.(*ast.AutoLink); ok {
 			url := autoLink.URL(source)
 			wrappedUrl := fmt.Sprintf("|%s|", url)
 			urls = append(urls, wrappedUrl)
-			if logger.V(1).Enabled() {
-				logger.V(1).Info("Found AutoLink", "url", string(url), "wrapped", wrappedUrl)
-			}
+			logger.V(1).Info("Found AutoLink", "url", string(url), "wrapped", wrappedUrl)
 		}
 
 		return ast.WalkContinue, nil
@@ -103,45 +98,35 @@ func Example(logger logr.Logger) {
 		return
 	}
 
-	if logger.V(1).Enabled() {
-		finalAST, err := dumpAST(doc)
-		if err != nil {
-			logger.Error(err, "Failed to dump final AST")
-			return
-		}
-		logger.V(1).Info("Final AST structure", "structure", finalAST)
+	finalAST, err := dumpAST(doc)
+	if err != nil {
+		logger.Error(err, "Failed to dump final AST")
+		return
 	}
+	logger.V(1).Info("Final AST structure", "structure", finalAST)
 
-	logger.V(1).Info("Found URLs", "count", len(urls))
+	logger.Info("Found URLs", "count", len(urls))
 	for i, url := range urls {
-		logger.V(1).Info(fmt.Sprintf("URL %d: %s", i+1, url))
+		logger.Info(fmt.Sprintf("URL %d: %s", i+1, url))
 	}
 
 	var buf bytes.Buffer
-	if logger.V(1).Enabled() {
-		logger.V(1).Info("Starting markdown rendering")
-	}
+	logger.V(1).Info("Starting markdown rendering")
 	if err := md.Renderer().Render(&buf, source, doc); err != nil {
 		logger.Error(err, "Error rendering markdown")
 		return
 	}
-	if logger.V(1).Enabled() {
-		logger.V(1).Info("Finished markdown rendering")
-	}
+	logger.V(1).Info("Finished markdown rendering")
 
 	output := buf.String()
-	if logger.V(1).Enabled() {
-		logger.V(1).Info("Rendered output", "output", output)
-	}
+	logger.V(1).Info("Rendered output", "output", output)
 
 	if err := os.WriteFile("testdata/output.md", buf.Bytes(), 0o644); err != nil {
 		logger.Error(err, "Error writing output file")
 		return
 	}
 
-	if logger.V(1).Enabled() {
-		logger.V(1).Info("Debug: Exiting Example function")
-	}
+	logger.V(1).Info("Debug: Exiting Example function")
 }
 
 func dumpAST(n ast.Node) (string, error) {
