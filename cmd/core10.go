a496fea79664308fae5aed36dd926e449a5089d1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-07-23 19:30:54 -0700

    core10

---
+++ cmd/core10.go
@@ -0,0 +1,28 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/gkwa/bravewaldo/core10"
+	"github.com/spf13/cobra"
+)
+
+// core10Cmd represents the core10 command
+var core10Cmd = &cobra.Command{
+	Use:   "core10",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		core10.Main()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(core10Cmd)
+}
---
+++ core10/main.go
@@ -0,0 +1,50 @@
+package core10
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"regexp"
+
+	markdown "github.com/teekennedy/goldmark-markdown"
+	"github.com/yuin/goldmark"
+	"mvdan.cc/xurls/v2"
+)
+
+var (
+	inputFilename  = "testdata/input.md"
+	outputFilename = "testdata/output.md"
+)
+
+func processURLs(input []byte) []byte {
+	rxStrict := xurls.Strict()
+	mdLinkRegex := regexp.MustCompile(`\[([^\]]+)\]\(([^)]+)\)`)
+	protected := mdLinkRegex.ReplaceAllFunc(input, func(match []byte) []byte {
+		return bytes.Replace(match, []byte("("), []byte("(protected:"), 1)
+	})
+	processed := rxStrict.ReplaceAllFunc(protected, func(match []byte) []byte {
+		return match
+	})
+	return mdLinkRegex.ReplaceAllFunc(processed, func(match []byte) []byte {
+		return bytes.Replace(match, []byte("(protected:"), []byte("("), 1)
+	})
+}
+
+func Main() {
+	source, err := os.ReadFile(inputFilename)
+	if err != nil {
+		log.Fatalf("Error reading input file: %v", err)
+	}
+	processedSource := processURLs(source)
+	md := goldmark.New(
+		goldmark.WithRenderer(markdown.NewRenderer()),
+	)
+	var buf bytes.Buffer
+	if err := md.Convert(processedSource, &buf); err != nil {
+		log.Fatalf("Error converting markdown: %v", err)
+	}
+	output := buf.String()
+	if err := os.WriteFile(outputFilename, []byte(output), 0o644); err != nil {
+		log.Fatalf("Error writing output file: %v", err)
+	}
+}
